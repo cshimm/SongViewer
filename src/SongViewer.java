a25a3c293645f6b9d96658a1b72c6de3ce681d57
cshimm <dameepman@gmail.com>
2023-10-24 18:35:17 -0700

    SongViewer: Basic integration with SongManager

--- src/SongViewer.java
+++ src/SongViewer.java
@@ -5,20 +5,28 @@ import com.opencsv.exceptions.CsvValidationException;
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class SongViewer extends JFrame {
-    static final int WIDTH = 350;
-    static final int HEIGHT = 400;
+    static final int WIDTH = 400;
+    static final int HEIGHT = 250;
+    static SongManager sm;
+    static int yearIndex;
+    static int songIndex;
 
-    public SongViewer() {
+    public SongViewer(String title) {
+        setTitle(title);
         setSize(WIDTH, HEIGHT);
-        setLayout(new GridLayout(2, 1));
+        setLayout(new GridLayout(1, 1));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public static class DataContainer extends JPanel {
         JButton[] buttons;
         JLabel detailsLabel;
+        JComboBox<String> yearDropdown;
 
         JButton DrawButton(String label) {
             JButton button = new JButton(label);
@@ -26,54 +34,112 @@ public class SongViewer extends JFrame {
             return button;
         }
 
-        public DataContainer(SongManager sm) {
-            GridLayout gridLayout = new GridLayout(2, 3);
+        public DataContainer() {
+            GridBagLayout gridLayout = new GridBagLayout();
             setLayout(gridLayout);
+            GridBagConstraints gbc = new GridBagConstraints();
             buttons = new JButton[3];
             buttons[0] = DrawButton("Load Data");
             buttons[1] = DrawButton("Prev");
             buttons[2] = DrawButton("Next");
-            for (var button : buttons) {
-                add(button);
-            }
-            JComboBox<String> yearDropdown = new JComboBox<>(sm.getYears());
-            add(yearDropdown);
-            detailsLabel = new JLabel("TESTSETESTESTSE");
+            for (int i = 0; i < buttons.length; i++) {
+                gbc.gridx = i;
+                gbc.gridy = 0;
+                if (i == 0)
+                    gbc.weightx = 0.25;
+                else
+                    gbc.weightx = 0.5;
 
-            add(detailsLabel);
+                gbc.fill = GridBagConstraints.HORIZONTAL;
+                add(buttons[i], gbc);
+            }
+            detailsLabel = new JLabel("");
+            detailsLabel.setPreferredSize(new Dimension(200, 30));
+            yearDropdown = new JComboBox<>();
+            gbc.gridx = 0;
+            gbc.gridy = 1;
+            add(yearDropdown, gbc);
+            gbc.gridx = 1;
+            gbc.gridwidth = 2;
+            add(detailsLabel, gbc);
         }
 
-        void setDetailsLabel(int currSongIndex, int totalSongs) {
-            detailsLabel.setText(String.valueOf(currSongIndex / totalSongs));
+        public void setData() {
+            for (var year : sm.getYears()) {
+                yearDropdown.addItem(year);
+            }
+            yearIndex = 0;
+            setDetailsLabel(songIndex, sm.getSongCount(yearIndex));
+        }
+
+        public void setDetailsLabel(int currSongIndex, int totalSongs) {
+            DecimalFormat df = new DecimalFormat("#.##");
+            int currSongNumber = currSongIndex + 1;
+            String formatted = df.format((currSongNumber / (double) totalSongs) * 100);
+            detailsLabel.setText("    " + formatted + "% | " + currSongNumber + " of " +
+                    sm.getSongCount(yearIndex) + " total songs");
+        }
+
+        public void handlePrevBtnClicked() {
+            if (songIndex <= 0)
+                songIndex = sm.getSongCount(yearIndex);
+            songIndex--;
+            setDetailsLabel(songIndex, sm.getSongCount(yearIndex));
+        }
+
+        public void handleNextBtnClicked() {
+            if (songIndex >= sm.getSongCount(yearIndex) - 1)
+                songIndex = -1;
+            songIndex++;
+            setDetailsLabel(songIndex, sm.getSongCount(yearIndex));
         }
     }
 
     public static class SongDetailsContainer extends JPanel {
+        SongDetail trackName;
+        SongDetail artist;
+        SongDetail releaseDate;
+        SongDetail totalStreams;
+
         public SongDetailsContainer() {
             GridLayout gridLayout = new GridLayout(4, 2);
             setLayout(gridLayout);
-            SongDetail trackName = new SongDetail("Track Name: ");
-            SongDetail artist = new SongDetail("Artist(s): ");
-            SongDetail releaseDate = new SongDetail("Release Date: ");
-            SongDetail totalStreams = new SongDetail("Total Streams: ");
-            add(trackName);
-            add(artist);
-            add(releaseDate);
-            add(totalStreams);
+
+            add(trackName = new SongDetail("Track Name: "));
+            add(artist = new SongDetail("Artist(s): "));
+            add(releaseDate = new SongDetail("Release Date: "));
+            add(totalStreams = new SongDetail("Total Streams: "));
+        }
+
+        public void setData() {
+            Song currentSong = sm.getSong(yearIndex, songIndex);
+            trackName.setTextField(currentSong.trackName());
+            artist.setTextField(currentSong.artistName());
+            String formattedReleaseDate = currentSong.releasedMonth() + "/" +
+                    currentSong.releasedDay() + "/" + currentSong.releasedYear();
+            releaseDate.setTextField(formattedReleaseDate);
+
+            NumberFormat nf = NumberFormat.getNumberInstance(Locale.US);
+            try {
+                String formattedStreams = nf.format(
+                        Double.parseDouble(currentSong.totalNumberOfStreamsOnSpotify()));
+                totalStreams.setTextField(formattedStreams);
+            } catch (NumberFormatException e) {
+                totalStreams.setTextField("Error: Bad data");
+            }
         }
 
         public static class SongDetail extends JPanel {
             JTextField textField;
 
             public SongDetail(String label) {
+                setLayout(new GridBagLayout());
                 GridBagConstraints textFieldConstraints = new GridBagConstraints();
+                add(new JLabel(label));
                 textFieldConstraints.weightx = 1.0;
                 textFieldConstraints.anchor = GridBagConstraints.EAST;
-                setLayout(new GridBagLayout());
-                JLabel jLabel = new JLabel(label);
                 textField = new JTextField();
-                textField.setPreferredSize(new Dimension(200, 30));
-                add(jLabel);
+                textField.setPreferredSize(new Dimension(250, 30));
                 add(textField, textFieldConstraints);
             }
 
@@ -83,22 +149,50 @@ public class SongViewer extends JFrame {
         }
     }
 
-    public static void main(String[] args) throws IOException, CsvValidationException {
-        SongManager sm = new SongManager();
-        SongViewer songViewer = new SongViewer();
-        DataContainer dataContainer = new DataContainer(sm);
-
-        dataContainer.buttons[0].addActionListener(actionEvent -> {
-
-        });
-        dataContainer.buttons[1].addActionListener((actionEvent) -> System.out.println("Prev button"));
-        dataContainer.buttons[2].addActionListener((actionEvent) -> System.out.println("next button"));
-
-
+    public static void main(String[] args) {
+        // Construct the UI
+        SongViewer songViewer = new SongViewer("Song Viewer");
+        JPanel container = new JPanel();
+        DataContainer dataContainer = new DataContainer();
         SongDetailsContainer songDetailsContainer = new SongDetailsContainer();
 
-        songViewer.add(dataContainer);
-        songViewer.add(songDetailsContainer);
+        // Listener for when new year is selected
+        dataContainer.yearDropdown.addActionListener(e -> {
+            yearIndex = dataContainer.yearDropdown.getSelectedIndex();
+            songIndex = 0;
+            boolean hasMultipleSongsInYear = sm.getSongCount(yearIndex) > 1;
+            dataContainer.buttons[1].setEnabled(hasMultipleSongsInYear);
+            dataContainer.buttons[2].setEnabled(hasMultipleSongsInYear);
+
+            dataContainer.setDetailsLabel(songIndex, sm.getSongCount(yearIndex));
+            songDetailsContainer.setData();
+        });
+        //Load button
+        dataContainer.buttons[0].addActionListener(e -> {
+            try {
+                sm = new SongManager();
+                dataContainer.setData();
+                songDetailsContainer.setData();
+                dataContainer.buttons[0].setEnabled(false);
+            } catch (IOException | CsvValidationException err) {
+                throw new RuntimeException(err);
+            }
+        });
+        // Prev btn
+        dataContainer.buttons[1].addActionListener(e -> {
+            dataContainer.handlePrevBtnClicked();
+            songDetailsContainer.setData();
+        });
+        // next btn
+        dataContainer.buttons[2].addActionListener(e -> {
+            dataContainer.handleNextBtnClicked();
+            songDetailsContainer.setData();
+        });
+
+        // Finally, add the UI to the Frame
+        container.add(dataContainer);
+        container.add(songDetailsContainer);
+        songViewer.add(container);
 
         songViewer.setVisible(true);
     }
